3e15002a7e6f7db951ad4ccee40cd481f036a582
Paul Ryan <paul.walker.ryan@gmail.com>
2023-07-08 15:05:10 -0600

    added comments to menu2 to explain everything

--- menu2.js
+++ menu2.js
@@ -1,16 +1,20 @@
-//Albums and songs on Albums
+//Menu program to display albums songs and artists. Also added play time for songs and run time for albums.
+
+//created song class to add song name, number of minutes and seconds in the song.
 class Song {
     constructor (name, minutes, seconds) {
         this.name = name;
         this.minutes = minutes;
         this.seconds = seconds;
     }
-
+// describe method to show the name of the song and how long it is. in order to display the seconds correctly, had to convert it 
+//to a string and used .padStart to make sure it is two characters long - will add zero to beginning of single digit values.
     describe() {
         return `${this.name} is ${this.minutes}:${this.seconds.toString().padStart(2, '0')} long`;
     }
 }
-
+// created Album class to create Album objects. Constructor accepts name and artist for parameters. Also added emply array for songs
+//to be added by addSong function. Added totalRunTime object to keep track of minutes and seconds for album run time.
 class Album {
     constructor (name, artist) {
         this.name = name;
@@ -22,12 +26,14 @@ class Album {
         };
         } ;
     
-
+//add song method to add songs to this.songs array. Adding song minutes and seconds to total run time.
     addSong(song) {
         if (song instanceof Song) {
             this.songs.push(song);
             this.totalRunTime.minutes += song.minutes;
             this.totalRunTime.seconds += song.seconds;
+            //had to add another if statment to account for seconds going over 60. If that happens, we take the seconds / 60 and then use math.floor
+            //to round down to the integer to add it to minutes. Then we add what ever remains after 60 seconds to the totalRuntime.seconds using %=.
             if (this.totalRunTime.seconds >= 60) {
                 this.totalRunTime.minutes += Math.floor(this.totalRunTime.seconds / 60);
                 this.totalRunTime.seconds %= 60; 
@@ -37,18 +43,18 @@ class Album {
             Argument is not a song: ${song}`);
         };
     };
-
+//describe method to give information about the album.
     describe() {
         return `${this.name} by ${this.artist} has ${this.songs.length} songs. The total run time is ${this.totalRunTime.minutes}:${this.totalRunTime.seconds.toString().padStart(2, '0')}.`;
     }
 }
-
+//created menu class.
 class Menu {
     constructor() {
         this.albums = [];
         this.selectedAlbum = null;
     }
-
+//used switch to create menu option selections.
     start() {
         let selection = this.showMainMenuOptions();
         while (selection !=0 ) {
@@ -71,7 +77,7 @@ class Menu {
         }
         alert('Goodbye!');
     }
-
+//created main menu options.
     showMainMenuOptions() {
         return prompt(`
         0) exit
@@ -81,7 +87,7 @@ class Menu {
         4) display all albums
         `)
     }
-
+//created album menu options.
     showAlbumMenuOptions(albumInfo) {
         return prompt(`
         0) back
@@ -91,7 +97,7 @@ class Menu {
         ${albumInfo}
         `);
     }
-
+//created display albums method.
     displayAlbums() {
         let albumString = '';
         for (let i = 0; i < this.albums.length; i++) {
@@ -103,13 +109,13 @@ class Menu {
         }
         alert(albumString)
     }
-
+//created create album method.
     createAlbum() {
         let name = prompt(`Enter name for a new album:`)
         let artist = prompt(`Enter the artist:`)
         this.albums.push(new Album(name, artist));
     }
-
+//created view album method.
     viewAlbum() {
         let index = prompt("Enter the index of the album that you want to view:");
         if (index > -1 && index < this.albums.length) {
@@ -130,21 +136,26 @@ class Menu {
             }
         }
     }
-
+//created deleteAlbum method. Will move an album from the albums array using splice method.
     deleteAlbum() {
         let index = prompt('Enter the index of the album you wish to delete: ');
         if (index > -1 && index < this.albums.length) {
             this.albums.splice(index,1);
         }
     }
-
+//created create song method. Since prompts always return a string, used parseInt to make sure minutes and seconds returned an integer/
     createSong() {
         let name = prompt('Enter name for song: ');
         let minutes = parseInt(prompt('Enter how many minutes the song has: '));
         let seconds = parseInt(prompt('Enter how many seconds the song has: '));
         this.selectedAlbum.addSong(new Song(name, minutes, seconds));
     }
-
+//created delete song method - needed to also have a way to reduce the run time of the album if a song was deleted. had to create a 
+//variable to store the deleted song and information (deletedSong). Then removed the song from the album using .splice. Next I had 
+//to subtract the deleted song minutes and seconds from the album. Needed to add an additional if statement so I wouldn't have negative
+//seconds so if the total run time for the album is less than 0, the if statment runs. Used math.ceil to round the number up to nearest integer.
+//used math.abs to make sure a positive number is returned and then divided that by 60 to give how many minutes to subtract. 
+//next we refigure the total run time seconds by taking 60 and subtracting whatever is remaning of the total run time seconds after 60.
     deleteSong() {
         let index = prompt('Enter the index of the song that you wish to delete: ');
         if (index > -1 && index < this.selectedAlbum.songs.length) {
